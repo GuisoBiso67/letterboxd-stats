1fad863238c0b3bc4bf8fc5203b27566b0f63499
Guilherme <guisobiso@gmail.com>
2026-09-01 17:45:52 -0300

    feat: tmdb integration

--- src/app/models/movie.model.ts
+++ src/app/models/movie.model.ts
@@ -1,7 +1,27 @@
-export interface Movie{
-title: string;
-release_year: number;
-log_date: Date;
-rating: number; // converter para inteiro na hora que ler do csv e dividir por 10 depois para exibir na pagina;
-letterboxd_URL: string;
+export interface Movie extends Partial<TmdbMovieDetails>{
+  title: string;
+  release_year: number;
+  log_date: Date;
+  rating: number; // converter para inteiro na hora que ler do csv e dividir por 10 depois para exibir na pagina;
+  letterboxd_URL: string;
+}
+// Partial<T> permite que os novos campos (de TmdbMovieDetails) sejam todos opcionais. Isso é perfeito pois nao teremos aqueles dados incialmente;
+
+export interface TmdbSearchResponse {
+  results: TmdbMovie[];
+}
+
+export interface TmdbMovie {
+  id: number;
+  title: string;
+  release_date: string;
+}
+
+export interface TmdbMovieDetails {
+  id: number;
+  genres: { id: number; name: string }[];
+  production_countries: { iso_3166_1: string; name: string }[];
+  credits: {
+    cast: { id: number; name: string; character: string }[];
+  };
 }
--- src/app/services/tmdb.ts
+++ src/app/services/tmdb.ts
@@ -1,8 +1,43 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Observable, switchMap, map } from 'rxjs';
+import { Movie, TmdbMovieDetails, TmdbSearchResponse } from '../models/movie.model';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class Tmdb {
-  
+@Injectable({ providedIn: 'root' })
+
+export class TmdbService {
+  private http = inject(HttpClient);
+  private apiKey = environment.tmdbApiKey;
+  private baseUrl = 'https://api.themoviedb.org/3';
+
+  searchMovie(title: string, year: number): Observable<TmdbSearchResponse> {
+    const full_url = `${this.baseUrl}/search/movie`;
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('query', title)
+      .set('primary_release_year', year)
+
+    return this.http.get<TmdbSearchResponse>(full_url, { params });
+  }
+
+  getMovieDetails(id: number): Observable<TmdbMovieDetails>{
+    const credits_url = `${this.baseUrl}/movie/${id}?append_to_response=credits`;
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+
+    return this.http.get<TmdbMovieDetails>(credits_url, { params });
+  }
+
+  enrichMovie(movie: Movie): Observable<Movie> {
+    return this.searchMovie(movie.title, movie.release_year).pipe(
+      switchMap(response => {
+        const id = response.results[0].id;
+        return this.getMovieDetails(id);
+      }),
+      map(details => {
+        return { ...movie, ...details };
+      })
+    );
+  }
 }
