4115d2250024ee63d95fb65b8448cb186671783f
Guilherme <guisobiso@gmail.com>
2026-09-01 15:34:15 -0300

    titles in HTML working

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { CsvParser } from '../../services/csv-parser';
 import { Movie } from '../../models/movie.model';
 
@@ -10,16 +10,18 @@ import { Movie } from '../../models/movie.model';
 })
 export class Home {
   private csvParser = inject(CsvParser);
-  movies: Movie[] = [];
+  movies = signal<Movie[]>([]);
 
   async onFileSelected(event: Event): Promise<void> {
     const input = event.target as HTMLInputElement;
+
     if (input && input.files && input.files.length > 0) {
       const file: File = input.files[0];
+
       try{
         const movieList: Movie[] = await this.csvParser.parse(file);
         console.log('Filmes importados com sucesso:', movieList);
-        this.movies = movieList;
+        this.movies.set(movieList);
       } catch(error){
         console.error('Erro ao processar arquivo csv: ', error);
       }
