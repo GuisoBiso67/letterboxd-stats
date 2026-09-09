6ad342658b88eedbb988589dce3d192c79175458
Guilherme <guisobiso@gmail.com>
2026-09-09 15:01:23 -0300

    feat: return computed/filtered data

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,8 +1,9 @@
 import { Component, inject, signal } from '@angular/core';
 import { CsvParser } from '../../services/csv-parser';
-import { Movie } from '../../models/movie.model';
+import { Movie, StatsList } from '../../models/movie.model';
 import { TmdbService } from '../../services/tmdb';
 import { from, concatMap, delay } from 'rxjs';
+import { Stats } from '../../services/stats';
 
 @Component({
   selector: 'app-home',
@@ -13,7 +14,9 @@ import { from, concatMap, delay } from 'rxjs';
 export class Home {
   private csvParser = inject(CsvParser);
   private tmdbService = inject(TmdbService);
+  private statsService = inject(Stats);
   movies = signal<Movie[]>([]);
+  stats_data = signal<StatsList | null>(null);
 
   async onFileSelected(event: Event): Promise<void> {
     const input = event.target as HTMLInputElement;
@@ -23,10 +26,10 @@ export class Home {
 
       try{
         const movieList: Movie[] = await this.csvParser.parse(file);
-        console.log('Filmes importados com sucesso:', movieList);
+        console.log('Successfully imported movies:', movieList);
         this.movies.set(movieList);
       } catch(error){
-        console.error('Erro ao processar arquivo csv: ', error);
+        console.error('Error processing csv file: ', error);
       }
     }
   }
@@ -36,7 +39,7 @@ export class Home {
       concatMap(movie => this.tmdbService.enrichMovie(movie).pipe(delay(250)))
     ).subscribe({
       next: (enrichedMovie: Movie) => {
-        console.log('Filme enriquecido:', enrichedMovie);
+        console.log('Movie enriched:', enrichedMovie);
         // alterar isso após a chegado dos IDs únicos;
         this.movies.update(movies => movies.map(item => 
           item.title === enrichedMovie.title && item.release_year === enrichedMovie.release_year 
@@ -44,7 +47,12 @@ export class Home {
             : item
         ));
       },
-      error: (err) => console.error(err)
+      error: (err) => console.error(err),
+      complete: () => {
+        console.log('Enrichment completed successfully!');
+        this.stats_data.set(this.statsService.compute(this.movies()));
+        console.log('Stats: ', this.stats_data());
+      }
     });
   }
 }
