208ef8010763a3ee9a57d143a58ca2a79c2b3ba5
Guilherme <guisobiso@gmail.com>
2026-09-14 15:56:13 -0300

    feat: directors page with top 4 cards and top 20 list

--- src/app/pages/directors/directors.ts
+++ src/app/pages/directors/directors.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { State } from '../../services/state';
 
 @Component({
   imports: [],
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
   templateUrl: './directors.html',
 })
 export class Directors {
+  readonly stateService = inject(State);
+
+  get top4() {
+    return this.stateService.stats_data()?.directors.slice(0, 4) ?? [];
+  }
+
+  get rest() {
+    return this.stateService.stats_data()?.directors.slice(4, 20) ?? [];
+  }
 }
