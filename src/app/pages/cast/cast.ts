987c23c0d414e03bc2541f4a2886c1af37567fae
Guilherme <guisobiso@gmail.com>
2026-09-14 16:03:43 -0300

    feat: cast page with top 4 cards and top 20 list

--- src/app/pages/cast/cast.ts
+++ src/app/pages/cast/cast.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { State } from '../../services/state';
 
 @Component({
   imports: [],
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
   templateUrl: './cast.html',
 })
 export class Cast {
+  readonly stateService = inject(State);
+
+  get top4() {
+    return this.stateService.stats_data()?.cast.slice(0, 4) ?? [];
+  }
+
+  get rest() {
+    return this.stateService.stats_data()?.cast.slice(4, 20) ?? [];
+  }
 }
